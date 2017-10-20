aa9083ba2469ac89a0ca4158a35b929aabf7c05a
Mustafa GUNES <gunes149@gmail.com>
2017-10-20 21:14:50 +0300

    Aşama6: form.html oluşturuldu, POST metodu ile veri akışı sağlandı

--- post/models.py
+++ post/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=120,verbose_name='Başlık')
     content = models.TextField(verbose_name='İçerik')
-    publishing_date = models.DateTimeField(verbose_name='Yayımlama Tarihi')
+    publishing_date = models.DateTimeField(verbose_name='Yayımlama Tarihi', auto_now_add=True)
 
     def __str__(self):
         return self.title
