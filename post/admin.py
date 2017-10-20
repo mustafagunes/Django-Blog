14613ff1f668d259e6a11d9fccccbbebfddb94ab
Mustafa GUNES <gunes149@gmail.com>
2017-10-20 14:47:20 +0300

    Aşama2: postuyglmasında sayfalara url verildi

--- post/admin.py
+++ post/admin.py
@@ -4,6 +4,10 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     
     list_display = ['title', 'publishing_date']
+    list_display_links = ['publishing_date']
+    list_filter = ['publishing_date']
+    search_fields = ['title', 'content']
+    list_editable = ['title']
 
     class Meta:
         model = Post
--- post/views.py
+++ post/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def post_index(request):
+    return HttpResponse('Burası Post index Sayfası !')
+
+
+def post_detail(request):
+    return HttpResponse('Burası Post detail Sayfası !')
+
+
+def post_create(request):
+    return HttpResponse('Burası Post create Sayfası !')
+
+
+def post_update(request):
+    return HttpResponse('Burası Post update Sayfası !')
+
+
+def post_delete(request):
+    return HttpResponse('Burası Post delete Sayfası !')
