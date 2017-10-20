820185bb10f3c4f8952fccca51884c39975dd7d4
Mustafa GUNES <gunes149@gmail.com>
2017-10-20 17:41:46 +0300

    Aşama4: index & detail sayfaları eklendi

--- post/views.py
+++ post/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Post
 
 
 def post_index(request):
-    return HttpResponse('Burası Post index Sayfası !')
+    posts = Post.objects.all()
+    return render(request, 'post/index.html', {'posts': posts})
 
 
 def post_detail(request):
-    return HttpResponse('Burası Post detail Sayfası !')
+    post = get_object_or_404(Post, id=3)
+    context = {
+        'post': post,
+    }
+    return render(request, 'post/detail.html', context)
 
 
 def post_create(request):
