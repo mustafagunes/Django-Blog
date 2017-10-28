0252af3451e817e5c5462acb712b4eece4193758
Mustafa GUNES <gunes149@gmail.com>
2017-10-28 20:20:17 +0300

    Aşama25: Kullanıcı çıkışı sağlandı

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,8 @@ app_name = 'accounts'
 urlpatterns = [
 
     url(r'^login/$', login_view, name='login'),
-    
+
     url(r'^register/$', register_view, name='register'),
+
+    url(r'^logout/$', logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm, RegisterForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 def login_view(request):
@@ -27,3 +27,8 @@ def register_view(request):
         login(request, new_user)
         return redirect('home')
     return render(request, 'accounts/form.html',{'form': form, 'title': 'Üye Ol'})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
